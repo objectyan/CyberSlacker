7017ba4f9ab3812270b708e6c937cdd54d4964df
OY <objecttozero@sina.com>
2026-03-19 13:16:45 +0800

    添加全局异常日志

--- CyberSlacker/App.xaml.cs
+++ CyberSlacker/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using AutoUpdaterDotNET;
+using CyberSlacker.Services;
+using Serilog;
 using System.Configuration;
 using System.Data;
 using System.IO;
@@ -14,6 +16,17 @@ namespace CyberSlacker
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            LogService.Initialize();
+            AppDomain.CurrentDomain.UnhandledException += (s, ev) =>
+            {
+                Log.Fatal(ev.ExceptionObject as Exception, "【致命错误】非UI线程崩溃");
+                Log.CloseAndFlush();
+            };
+            this.DispatcherUnhandledException += (s, ev) =>
+            {
+                Log.Error(ev.Exception, "【UI错误】拦截到未处理的异常");
+                ev.Handled = true;
+            };
             // 强制软件渲染（如果显卡驱动在跨屏时有 Bug，开启这个反而会变流畅）
             System.Windows.Media.RenderOptions.ProcessRenderMode = System.Windows.Interop.RenderMode.SoftwareOnly;
             base.OnStartup(e);
@@ -31,6 +44,13 @@ namespace CyberSlacker
 
             string arch = RuntimeInformation.ProcessArchitecture.ToString().ToLower();
             return $"https://raw.githubusercontent.com/objectyan/CyberSlacker/master/Update_{arch}.xml";
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.Information(">>> 赛博摸鱼员 正常关闭 <<<");
+            Log.CloseAndFlush(); 
+            base.OnExit(e);
         }
     }
 
---
+++ CyberSlacker/Services/LogService.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using System.IO;
+
+namespace CyberSlacker.Services
+{
+    public static class LogService
+    {
+        // 存放路径：C:\Users\用户名\AppData\Local\CyberSlacker\Logs
+        private static readonly string LogPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "CyberSlacker", "Logs", "log-.txt");
+
+        public static void Initialize()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(
+                    LogPath,
+                    rollingInterval: RollingInterval.Day, // 每天一个新文件
+                    retainedFileCountLimit: 7,            // 只保留最近7天
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+                )
+                .CreateLogger();
+
+            Log.Information(">>> 赛博摸鱼员 启动 <<<");
+        }
+    }
+}
