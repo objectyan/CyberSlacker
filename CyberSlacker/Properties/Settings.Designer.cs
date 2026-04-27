db560a8dd4783d732484c62077d4ff67b6fc7504
OY <objecttozero@sina.com>
2026-04-27 13:08:38 +0800

    fix 优化透明度显示bug

--- CyberSlacker/Properties/Settings.Designer.cs
+++ CyberSlacker/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CyberSlacker.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -181,9 +181,9 @@ namespace CyberSlacker.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("0.8")]
-        public string Opacity {
+        public decimal Opacity {
             get {
-                return ((string)(this["Opacity"]));
+                return ((decimal)(this["Opacity"]));
             }
             set {
                 this["Opacity"] = value;
